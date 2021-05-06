0cb0f7ec30d0cd0b8077cd37cefe320a7b3d05ee
Maxl <36601278+maxlluky@users.noreply.github.com>
2021-05-06 22:14:41 +0200

    Update Localhost.cs

--- DhcpSharp/Classes/Localhost.cs
+++ DhcpSharp/Classes/Localhost.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 
 class Localhost
 {
-    //--Retrieve the device list from the local machine
     private static IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
-
-    //--Represents the active interface to use for the dhcp service
     private static PacketDevice packetDevice;
 
     public IList<LivePacketDevice> getUseableInterfaces()
