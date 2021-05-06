367cfcfa177ac0496204124389eabcb79153b076
Maxl <36601278+maxlluky@users.noreply.github.com>
2021-05-06 22:14:24 +0200

    Update Service.cs

--- DhcpSharp/Classes/Service.cs
+++ DhcpSharp/Classes/Service.cs
@@ -10,16 +10,11 @@ using System.Linq;
 
 class Service
 {
-    //--Classes
     private static Localhost localhost;
     private readonly Interface inter;
     private AddressPool addressPool;
     private Builder builder;
-
-    //--Active Device
     private PacketDevice packetDevice;
-
-    //--PacketCommunicator is used to send down and receive packets
     private PacketCommunicator packetCommunicator;
 
     public Service(Localhost pLocalhost, AddressPool pAddressPool, Interface pInter)
@@ -31,77 +26,55 @@ class Service
 
     public void startListen()
     {
-        //--Get active Interface/Device to use
         packetDevice = localhost.getActiveInterface();
-
-        // Open the device
         packetCommunicator = packetDevice.Open(65536, PacketDeviceOpenAttributes.Promiscuous, 1000);
-
+        
         Console.WriteLine("Listening on " + packetDevice.Description + "...");
         Console.WriteLine("Status\t\t\tDestination MAC\t\tDHCP Message\tTransaction ID\t\tServer Identifier");
         Console.WriteLine("===========================================================================================================");
-
-        //--Create Builderclass to build the networkpackets
+        
         builder = new Builder(addressPool, inter);
-
-        //--Set a filter to reduce the traffic
         packetCommunicator.SetFilter("udp");
-
-        // start the capture
         packetCommunicator.ReceivePackets(0, receiveCallback);
-
     }
 
-    // Callback function invoked by Pcap.Net for every incoming packet. Includes packets that are send over the device.
     private void receiveCallback(Packet packet)
     {
         try
         {
-            //--Parsing the layer above Ethernet
             IpV4Datagram ipPacket = packet.Ethernet.IpV4;
             UdpDatagram udpDatagram = ipPacket.Udp;
-
             Datagram datagram = udpDatagram.Payload;
-
-            //--Check if Packet is DHCP with Ports.
+            
             if (udpDatagram.SourcePort.Equals(68) & udpDatagram.DestinationPort.Equals(67))
             {
-                //--Create a new DhcpPacket to parse the received and read Data from it.
                 DHCPv4Packet dhcpv4Packet = new DHCPv4Packet();
-
                 if (dhcpv4Packet.parsePacket(datagram.ToArray()))
-                {
-                    //--Create a dhcpOption.             
+                {          
                     List<DHCPv4Option> list = new DHCPv4Option().parseDhcpOptions(dhcpv4Packet.dhcpOptions);
-
                     foreach (DHCPv4Option dhcpMessageTypeOption in list)
-                    {
-                        //--Checks if DHCP Packet contains DHCP Message Type-Option.
+                    {                        
                         if (dhcpMessageTypeOption.optionIdBytes.Equals(0x35))
                         {
                             switch (dhcpMessageTypeOption.optionValue[0])
                             {
-                                //--DHCP Packet is a Discover
+                                //--Packet is a Discover
                                 case 0x01:
-                                    Console.WriteLine("Service received:\t" + packet.Ethernet.Destination + "\tDISCOVER\txid: " + BitConverter.ToString(dhcpv4Packet.xid));
-
-                                    //--Sending an Dhcp Offer                             
+                                    Console.WriteLine("Service received:\t" + packet.Ethernet.Destination + "\tDISCOVER\txid: " + BitConverter.ToString(dhcpv4Packet.xid));                        
                                     sendDhcpOffer(new MacAddress(inter.getMacAddress()), packet.Ethernet.Source, dhcpv4Packet.xid, dhcpv4Packet.secs);
 
                                     break;
-                                //--DHCP Packet is an Request
+                                //--Packet is an Request
                                 case 0x03:
                                     foreach (DHCPv4Option dhcpServerIdentifierOption in list)
                                     {
-                                        //--Checks if the DHCP Packet contains a DHCP Server Identifier-Option.
+                                        //--DHCP contains Server-Identifier-Option.
                                         if (dhcpServerIdentifierOption.optionIdBytes.Equals(0x36))
                                         {
-                                            //--Checks if the DHCP Server Identifier equals the IP-Address of the DHCP-Server.
+                                            //--DHCP-Server-Identifier equals IP-Address of DHCP-Server.
                                             if (BitConverter.ToInt32(dhcpServerIdentifierOption.optionValue, 0).Equals(BitConverter.ToInt32(inter.getIPAddress().GetAddressBytes(), 0)))
                                             {
                                                 Console.WriteLine("Service received:\t" + packet.Ethernet.Destination + "\tREQUEST\t\txid: " + BitConverter.ToString(dhcpv4Packet.xid) + "\tSID: " + BitConverter.ToString(dhcpServerIdentifierOption.optionValue, 0));
-
-                                                //--Sending Dhcp Ack  
                                                 sendDhcpAck(new MacAddress(inter.getMacAddress()), packet.Ethernet.Source, dhcpv4Packet.xid, dhcpv4Packet.secs);
                                             }
                                             else
@@ -127,16 +100,12 @@ class Service
     public void sendDhcpOffer(MacAddress pSourceMacAddress, MacAddress pDestinationMacAddress, byte[] pTransactionId, byte[] pSecs)
     {
         Console.WriteLine("Service send:\t\t" + pDestinationMacAddress + "\tOFFER\t\txid: " + BitConverter.ToString(pTransactionId));
-
-        //--Send down the packet. First build the packet.
         packetCommunicator.SendPacket(builder.buildDhcpOffer(pSourceMacAddress, pDestinationMacAddress, pTransactionId, pSecs));
     }
 
     public void sendDhcpAck(MacAddress pSourceMacAddress, MacAddress pDestinationMacAddress, byte[] pTransactionId, byte[] pSecs)
     {
         Console.WriteLine("Service send:\t\t" + pDestinationMacAddress + "\tACK\t\txid: " + BitConverter.ToString(pTransactionId));
-
-        //--Send down the packet. First build the packet. The Method "buildDhcpAck" will decide if it sends a ACK or NAK.
         packetCommunicator.SendPacket(builder.buildDhcpAck(pSourceMacAddress, pDestinationMacAddress, pTransactionId, pSecs));
     }
 }
