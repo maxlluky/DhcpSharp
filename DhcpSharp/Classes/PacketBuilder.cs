44c521dc9f20dae42a126bc545346a2947ec9555
Maxl <maxarttm@gmail.com>
2021-11-12 16:47:42 +0100

    Fix

--- DhcpSharp/Classes/PacketBuilder.cs
+++ DhcpSharp/Classes/PacketBuilder.cs
@@ -70,11 +70,6 @@ class PacketBuilder
 
         dhcpv4Packet.SetOptions(dhcpOptionList);
 
-        //--Checksum
-        Debug.WriteLine("Calculating now Checksum");
-        udpPacket.Checksum = calculateUdpChecksum(dhcpIp, newClientIPAddress, udpPacket);
-        ipv4Packet.Checksum = 65535;
-
         //--Merge
         udpPacket.PayloadData = dhcpv4Packet.Bytes;
         ipv4Packet.PayloadPacket = udpPacket;
@@ -82,53 +77,7 @@ class PacketBuilder
 
         return ethernetPacket;
     }
-
-    private ushort calculateUdpChecksum(IPAddress pSourceIp, IPAddress pDestIp, UdpPacket pUdpPacket)
-    {
-        ushort checksum = 0;
-        byte[] checksumBytes;
-
-        using (MemoryStream memoryStream = new MemoryStream())
-        {
-            using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream))
-            {
-                Debug.WriteLine("SourceIp: {0}", pSourceIp.ToString());
-                Debug.WriteLine("DestIp: {0}", pDestIp.ToString());
-                Debug.WriteLine("Length: {0}", Convert.ToInt16(pUdpPacket.Length));
-                Debug.WriteLine("S-Port: {0}", pUdpPacket.SourcePort);
-                Debug.WriteLine("D-Port: {0}", pUdpPacket.DestinationPort);
-
-                binaryWriter.Write(pSourceIp.GetAddressBytes()); //--32-bit Source IP
-                binaryWriter.Write(pDestIp.GetAddressBytes()); //--32-bit Dest IP
-                binaryWriter.Write((ushort)17); //--16-bit Zeros and Protocol
-                binaryWriter.Write(Convert.ToInt16(pUdpPacket.Length)); //--16-bit UDP length
-                binaryWriter.Write(pUdpPacket.SourcePort); //--16bit Source Port
-                binaryWriter.Write(pUdpPacket.DestinationPort); //--16bit Dest Port
-                binaryWriter.Write(Convert.ToInt16(pUdpPacket.TotalPacketLength)); //--16-bit UDP length
-                // binaryWriter.Write(pUdpPacket.PayloadData); //--Data
-            }
-
-            checksumBytes = memoryStream.ToArray();
-        }
-
-        Debug.WriteLine("Got ChecksumBytes");
-        /*
-        for (int i=0; i<checksumBytes.Length; i++)
-        {
-            checksum += Convert.ToByte(checksumBytes[i] % 256);
-        }
-        */
-
-        for (int i=0; i<checksumBytes.Length; i++)
-        {
-            byte[] sixteen = { checksumBytes[i], checksumBytes[i++] };
-            checksum += BitConverter.ToUInt16(sixteen, 0);           
-        }            
-
-        Debug.WriteLine("Calculated Checksum: {0}", checksum);
-        return checksum;
-    }
-
+    
     public Packet buildDhcpAck(PhysicalAddress pSourceMacAddress, PhysicalAddress pDestinationMacAddress, uint pTransactionId, ushort pSecs, Subnet pSubnet)
     {
         IPAddress newClientIPAddress = IPAddress.Parse("0.0.0.0");
@@ -162,7 +111,7 @@ class PacketBuilder
         UdpPacket udpPacket = new UdpPacket(67, 68);
 
         //--Build DHCP
-        DhcpV4Packet dhcpv4Packet = new DhcpV4Packet(new ByteArraySegment(new byte[350]), null)
+        DhcpV4Packet dhcpv4Packet = new DhcpV4Packet(new ByteArraySegment(new byte[350]), udpPacket)
         {
             MessageType = DhcpV4MessageType.Ack,
             Operation = DhcpV4Operation.BootReply,
